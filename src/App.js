37c177bccfecd4434c60ff1f5c7e717308cede25
Jayaprakashs-AdapptLimited <jayaprakash.s@adappt.ai>
2024-02-19 19:42:35 +0530

    loading questions with API(fake)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,57 @@
-import './App.css';
-import DateCounter from './DateCounter';
-import Header from './Header';
+import { useEffect, useReducer } from "react";
+import "./App.css";
+import DateCounter from "./DateCounter";
+import Header from "./Header";
+import Main from "./Main";
+
+const initialState = {
+  questions: [],
+
+  // loading, error, ready, active, finished
+  status: "loading",
+};
+
+function reducer(state, action) {
+  // console.log(state);
+  console.log(action);
+  switch (action.type) {
+    case "dataRecieved":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+
+    case "dataFailed": return {
+      ...state,
+      status: "error",
+    };
+
+    default:
+      throw new Error("something wrong");
+  }
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const { questions, status } = state;
+
+  useEffect(function () {
+    fetch(`http://localhost:8000/questions`).then((res) =>
+      res
+        .json()
+        .then((data) => dispatch({type: "dataRecieved", payload: data }))
+        .catch((err) => dispatch({type: "dataFailed", payload: err }))
+    );
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <Header />
-      </header>
-      <DateCounter />
+    <div className="app">
+      <Header />
+      <Main className="main">
+        <p> Question 1 /15 </p>
+        <p> question ?</p>
+      </Main>
     </div>
   );
 }
